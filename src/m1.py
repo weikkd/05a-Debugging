ac5b096f2c7d523f4921267f4adbf6766163e9d9
weikkd <weikkd@rose-hulman.edu>
2018-12-05 10:35:29 -0500

    Done.

--- src/m1.py
+++ src/m1.py
@@ -239,7 +239,8 @@ def broken_3(n, point, length, distance_between_lines, window):
         line = rg.Line(a, b)
         line.attach_to(window)
         window.render(0.5)
-        b = b + distance_between_lines
+        a.x = a.x + distance_between_lines
+        b.x = b.x + distance_between_lines
 
 
 # -----------------------------------------------------------------------------
@@ -269,7 +270,7 @@ def broken_4(x, y, radius, window):
 
 
 # -----------------------------------------------------------------------------
-# TODO: 7. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 7. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # -----------------------------------------------------------------------------
 def broken_5(circle, window):
@@ -287,14 +288,14 @@ def broken_5(circle, window):
       :type window: rg.RoseWindow
     """
     circle.attach_to(window)
-    square = rg.Square(circle.center)
+    square = rg.Square(circle.center, circle.radius*2)
     square.outline_color = circle.fill_color
     square.attach_to(window)
     window.render()
 
 
 # -----------------------------------------------------------------------------
-# TODO: 8. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 8. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # -----------------------------------------------------------------------------
 def broken_6(n):
@@ -305,14 +306,13 @@ def broken_6(n):
     Side effects:   None.
     """
     total = 0
-    for k in range(n + 1):
-        total = total + 1 / k
-
+    for k in range(n):
+        total = total + 1 / (k+1)
     return total
 
 
 # -----------------------------------------------------------------------------
-# TODO: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
+# DONE: 9. Follow the INSTRUCTIONS AT THE TOP OF THIS MODULE
 #          to correct the mistake(s) in the following function.
 # -----------------------------------------------------------------------------
 def broken_7(n, point, length, distance_between_lines, window):
@@ -339,15 +339,14 @@ def broken_7(n, point, length, distance_between_lines, window):
       :type window:                 rg.RoseWindow
     """
     left = rg.Point(point.x, point.y)
-    right = rg.Point(point.x + length, point.y)
+    right = rg.Point((point.x + length), point.y)
 
     for _ in range(n):
         line = rg.Line(left, right)
         line.attach_to(window)
         window.render(0.5)
-        left = rg.Point(left.x, left.y + distance_between_lines)
-        right = rg.Point(right.x, right.y + distance_between_lines)
-    window.close_on_mouse_click()
+        left = rg.Point(left.x, (left.y + distance_between_lines))
+        right = rg.Point(right.x, (right.y + distance_between_lines))
 
 
 # -----------------------------------------------------------------------------
