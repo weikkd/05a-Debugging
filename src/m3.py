17a9332e3ad4f8a58fd4734e8fdd5df35f701427
weikkd <weikkd@rose-hulman.edu>
2018-12-05 23:47:33 -0500

    Done.

--- src/m3.py
+++ src/m3.py
@@ -123,9 +123,9 @@ def broken_1(m):
     #    **  For full credit you must appropriately
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     count = 0
-    for k in range(m + 1):
-        if is_prime(m):
-            k = True
+    for k in range((2*m)-m+2):
+        x = k + m
+        if is_prime(x) is True:
             count = count + 1
     return count
 
@@ -133,4 +133,5 @@ def broken_1(m):
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
-    main()
+
+main()
